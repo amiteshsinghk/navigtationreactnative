5616d4f8537795514caf95805a163329e68f6b60
amiteshkundan <amitesh.kundan@mckinleyrice.co>
2021-11-29 19:37:12 +0530

    api integration

--- App.js
+++ App.js
@@ -16,6 +16,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 import {
@@ -27,7 +28,7 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 // import Home from './screens/home';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './screens/home';
 import Review from './screens/review';
@@ -36,6 +37,12 @@ import Messages from './screens/messages';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Feed from './screens/feed';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
 
 // const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -43,27 +50,69 @@ const Tab = createBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
 
+const Drawer = createDrawerNavigator();
+
 function HomeStackScreen() {
   return (
     <Stack.Navigator>
-     <Stack.Screen name="Home" component={Home} options={{
-          title: 'My Home',
-          headerShown: false,
-          headerStyle: {
-            backgroundColor: '#77f0ff',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }} />
-        <Stack.Screen name="Review" component={Review} options ={{
-          headerShown: false
-        }} />
-        <Stack.Screen name="About" component={About} options ={{
-          headerShown: false
-        }}/>
+      <Stack.Screen name="Home" component={Home} options={{
+        title: 'My Home',
+        headerShown: false,
+        headerStyle: {
+          backgroundColor: '#77f0ff',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }} />
+      <Stack.Screen name="Review" component={Review} options={{
+        headerShown: false
+      }} />
+      <Stack.Screen name="About" component={About} options={{
+        headerShown: false
+      }} />
     </Stack.Navigator>
+  );
+}
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator initialRouteName="Home">
+      <Drawer.Screen name="Feed" component={HomeStackScreen} />
+      <Drawer.Screen name="Messages" component={Messages} />
+    </Drawer.Navigator>
+  );
+}
+function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Feed') {
+            iconName = focused
+              ? 'home'
+              : 'home-outline';
+          } else if (route.name === 'Messages') {
+            iconName = focused ? 'caret-back' : 'arrow-back-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={20} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Feed" component={HomeStackScreen}
+
+        options={{ tabBarBadge: 2, headerShown: true }} Ionicons />
+      <Tab.Screen name="Messages" component={Messages} />
+
+
+    </Tab.Navigator>
   );
 }
 
@@ -71,6 +120,26 @@ function HomeStackScreen() {
 const App: () => Node = () => {
   return (
     <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" component={TabNavigator} options={{
+          headerShown: false,
+          title: 'My Home',
+          // headerRight: () => (
+            // <Button
+            //   onPress={() => }
+            //   title="Info"
+            //   color="#fff"
+            // />
+          // ),
+        }}
+        />
+        <Stack.Screen name="Feed" component={DrawerNavigator} options={{
+          headerShown: false,
+          title: 'feed'
+        }} />
+
+      </Stack.Navigator>
       {/* <Stack.Navigator>
 
         <Stack.Screen name="Home" component={Home} options={{
@@ -86,8 +155,8 @@ const App: () => Node = () => {
         <Stack.Screen name="Review" component={Review} />
         <Stack.Screen name="About" component={About} />
       </Stack.Navigator> */}
-        <Tab.Navigator
-         screenOptions={({ route }) => ({
+      {/* <Tab.Navigator
+        screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
@@ -105,13 +174,30 @@ const App: () => Node = () => {
           tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
         })}
-        >
+      >
         <Tab.Screen name="Feed" component={HomeStackScreen}
-        options ={{tabBarBadge : 2,}} />
+          options={{ tabBarBadge: 2, }} />
         <Tab.Screen name="Messages" component={Messages} />
-      </Tab.Navigator>
+       
+       
+      </Tab.Navigator> */}
+
+
     </NavigationContainer>
-    // <Home/>
+    //     // <Home/>
   );
 };
+
+// const App: () => Node = () => {
+//   return (
+//     <NavigationContainer>
+//         <Drawer.Navigator initialRouteName="Home">
+//         <Drawer.Screen name="Feed" component={HomeStackScreen} />
+//         <Drawer.Screen name="Messages" component={Messages} />
+//       </Drawer.Navigator>
+
+//     </NavigationContainer>
+//   );
+// };
+
 export default App;
